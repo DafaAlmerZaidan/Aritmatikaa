68927ab70fd95d2595dae8dc81b5b5cf16047d34
DafaAlmerZaidan <144661273+DafaAlmerZaidan@users.noreply.github.com>
2023-09-11 14:44:44 +0700

    Create Aritmatika.cpp

---
+++ Aritmatika.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Program Penjumlahan \n";
+    cout<<"Masukkan Angka Pertama:";
+    cin>>a;
+    cout<<"masukkan Angka Kedua:";
+    cin>>b;
+    cout<<"hasil penjumlahan adalah:"<<a+b<<endl;
+    cout<<"hasil pengurangan adalah:"<<a-b<<endl;
+    cout<<"hasil perkalian adalah:"<<a*b<<endl;
+    cout<<"hasil pembagian adalah:"<<a/b<<endl;
+    cout<<"hasil sisa bagi adalah:"<<a%b<<endl;
+    return 0;
+}
